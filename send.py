77a86df114286e6a53d081820d5140a8cf7ac7f8
Paulo McNally <paulomcnally@gmail.com>
2015-08-01 20:48:45 -0600

    remove old ip implementation

--- send.py
+++ send.py
@@ -1,15 +1,10 @@
 import mandrill
 import sys
-import socket
-import json
 
 # sys.argv[1] = ip
 # sys.argv[2] = MANDRILL_API_KEY
 # sys.argv[3] = TO_EMAIL
 # sys.argv[3] = TO_NAME
-
-# load ips
-ips = json.dumps([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1])
 
 # send email
 try:
@@ -22,7 +17,6 @@ try:
         'text': sys.argv[1],
         'important': False,
         'tags': ['raspberry-pi-ip'],
-
         'to':
             [
                 {
