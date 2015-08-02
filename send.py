bad6c74d13eda306a94aee929697deda635a6f10
Paulo McNally <paulomcnally@gmail.com>
2015-08-01 20:06:46 -0600

    use arguments

--- send.py
+++ send.py
@@ -1,28 +1,33 @@
 import mandrill
-import os
+import sys
 import socket
 import json
+
+# sys.argv[1] = ip
+# sys.argv[2] = MANDRILL_API_KEY
+# sys.argv[3] = TO_EMAIL
+# sys.argv[3] = TO_NAME
 
 # load ips
 ips = json.dumps([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1])
 
 # send email
 try:
-    mandrill_client = mandrill.Mandrill(os.environ['MANDRILL_API_KEY'])
+    mandrill_client = mandrill.Mandrill(sys.argv[2])
     message = {
         'from_email': 'device@raspberrypi.org',
         'from_name': 'Raspberry PI',
         'subject': 'Raspberry PI - IP',
-        'html': ips,
-        'text': ips,
+        'html': sys.argv[1],
+        'text': sys.argv[1],
         'important': False,
         'tags': ['raspberry-pi-ip'],
 
         'to':
             [
                 {
-                    'email': os.environ['TO_EMAIL'],
-                    'name': os.environ['TO_NAME'],
+                    'email': sys.argv[3],
+                    'name': sys.argv[4],
                     'type': 'to'
                 }
             ],
